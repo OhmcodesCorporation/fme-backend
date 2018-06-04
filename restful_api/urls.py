f9a579bf3ee75893c249710cbc1919130a5cd70b
ohmcodes <ohmcodes@gmail.com>
2018-06-04 02:40:06 -0700

    Added Refresh and Verify token

--- restful_api/urls.py
+++ restful_api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from .views import *
 
 urlpatterns = [
 	url(r'^token/auth/$', obtain_jwt_token),
+	url(r'^token/refresh/$', refresh_jwt_token),
+	url(r'^token/verify/$', verify_jwt_token),
 	url(r'^login/$', UserLoginAPIView.as_view(), name='login'),
 	url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
 	url(r'^members/$', UserDetailAPIView.as_view(), name='members'),
